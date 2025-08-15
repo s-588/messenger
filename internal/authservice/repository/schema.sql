e5ebc72693f0a29eca077b67bff443efd1b2e5ab
s-588 <0000yy@proton.me>
2025-08-15 13:51:46 +0300

    add token table
    Add extension for UUID.
    
    Add default auto generated UUID for user_id. For simply Go logic and
    remove need in generating UUID by service.
    
    Add new table for storing refresh tokens.

--- internal/authservice/repository/schema.sql
+++ internal/authservice/repository/schema.sql
@@ -1,6 +1,22 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
 CREATE TABLE users(
-    user_id UUID PRIMARY KEY,
-    username varchar(255) UNIQUE NOT NULL,
-    password varchar(60) NOT NULL,
-    registered_at timestamp NOT NULL DEFAULT Now()
+    user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(60) NOT NULL,
+    registered_at TIMESTAMP NOT NULL DEFAULT now()
 );
+
+CREATE TABLE refresh_tokens (
+    token_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
+    token_hash TEXT NOT NULL,
+    issued_at TIMESTAMP NOT NULL DEFAULT now(),
+    expires_at TIMESTAMP NOT NULL,
+    device_info TEXT,
+    ip_address TEXT
+);
+
+CREATE INDEX refresh_tokens_token_hash_idx ON refresh_tokens(token_hash);
+CREATE INDEX refresh_tokens_user_id_idx ON refresh_tokens(user_id);
+
