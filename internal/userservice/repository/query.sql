ed7cf7965ea79d41679d6440f1865a9d061b1277
s-588 <0000yy@proton.me>
2025-08-16 20:47:13 +0300

    update user service queries
    Rename GetUserByEmail to GetUserDataByEmail and other function
    accordingly.

--- internal/userservice/repository/query.sql
+++ internal/userservice/repository/query.sql
@@ -1,19 +1,19 @@
--- name: GetUserByID :one
+-- name: GetUserDataByID :one
 SELECT first_name , last_name , email , avatar_url
-FROM users 
+FROM usersData 
 WHERE user_id = $1;
 
--- name: GetUserByEmail :one
+-- name: GetUserDataByEmail :one
 SELECT user_id, first_name , last_name , avatar_url
-FROM users 
+FROM usersData 
 WHERE email = $1;
 
--- name: DeleteUserByID :exec
-DELETE FROM users
+-- name: DeleteUserDataByID :exec
+DELETE FROM usersData
 WHERE user_id = $1;
 
--- name: CreateUser :one
-INSERT INTO users(
+-- name: CreateUserData :one
+INSERT INTO usersData(
     user_id, first_name, last_name, email, avatar_url
 ) VALUES(
     $1,$2,$3,$4,$5
@@ -21,17 +21,17 @@ INSERT INTO users(
 RETURNING *;
 
 -- name: UpdateUserFullName :exec
-UPDATE users
+UPDATE usersData
     set first_name = $2,
         last_name = $3
 WHERE user_id = $1;
 
 -- name: UpdateUserEmail :exec
-UPDATE users
+UPDATE usersData
     set email = $2
 WHERE user_id = $1;
 
 -- name: UpdateUserAvatar :exec
-UPDATE users
+UPDATE usersData
     set avatar_url = $2
 WHERE user_id = $1;
